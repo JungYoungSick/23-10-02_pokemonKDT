7c377161002f08090a8667799c5a6d23efdb9843
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-02 16:34:23 +0900

    1차 완료

--- KDT1.py
+++ KDT1.py
@@ -1,13 +1,10 @@
+# 1. 포켓몬스터 배열 '_pokemon'에 새로운 포켓몬을 추가해주세요. if나 for문을 사용하여 _pokemon 배열의 마지막에 새로운 포켓몬 이름을 추가하세요.
+
 pokemon = [
   "이상해씨", "이상해풀", "이상해꽃", "파이리", "꼬부기", "라이츄"]
 
-# 리스트의 길이를 구합니다.
-pokemon_length = len(pokemon)
-
-# 리스트의 각 요소를 순회하며 마지막 요소를 확인합니다.
-for i in range(len(pokemon)):
-  if i == len(pokemon)-1:
-    pokemon[i] = "영식몬"
+for i in range(len(pokemon)+1):
+  if i == len(pokemon):
+    pokemon.insert(i, "영식몬")
     print(i)
-
 print(pokemon)
